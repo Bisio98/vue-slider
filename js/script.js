87d90902605976384353b8cfba8748cce01c1792
Bisio98 <alberto.bisetti@hotmail.it>
2021-12-04 18:29:44 +0100

    completed

---
+++ js/script.js
@@ -0,0 +1,47 @@
+Vue.config.devtools = true;
+
+var app = new Vue({
+    el: '#root',
+    data: {
+        currentActiveSlide: 0,
+        slides: [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },{
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum'
+            },{
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+            },{
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+            },{
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+            }
+        ]
+    },
+    methods: {
+        showNextSlide: function() {
+            if(this.currentActiveSlide < this.slides.length - 1){
+                this.currentActiveSlide++;
+            }else{
+                this.currentActiveSlide = 0;
+            }
+        },   
+        showPrevSlide: function() {
+            if(this.currentActiveSlide > 0){
+                this.currentActiveSlide--;
+            }else{
+                this.currentActiveSlide = (this.slides.length - 1);
+            }
+        }
+    }
+});
